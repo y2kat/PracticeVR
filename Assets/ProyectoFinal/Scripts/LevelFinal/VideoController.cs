6e48ee801515a43bf3b99ac63be5d95d44d92eab
y2kat <lentacomputadora@gmail.com>
2024-05-13 22:16:31 -0600

    Game Finished

---
+++ Assets/ProyectoFinal/Scripts/LevelFinal/VideoController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        Debug.Log("exit :V");
+        Application.Quit();
+    }
+}
+
